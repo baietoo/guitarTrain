c46ae8d081962bb495195007c64749a1f32fd171
baietoo <catalingcircu@gmail.com>
2021-02-14 04:19:29 +0200

    separated string and note

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict'
-let divString = document.getElementById('string-nr-container');
+let divString = document.getElementById('string-number');
+let divNote = document.getElementById('string-note');
 let btnStartPractice = document.getElementById('start-practice');
 let btnConfirmPractice = document.getElementById('confirm-practice');
 let btnStopPractice = document.getElementById('stop-practice');
 let cardString = document.getElementById("string-card");
 let nrOfSeconds = document.getElementById("seconds");
+
 let practiceInterval = null;
 const stringNotes = [
     'A', 'A#', 'B', 'C', 'C#', 'D',
@@ -19,19 +21,24 @@ let interval = null;
 function startPractice() {
     while (divString.firstChild) {
         divString.removeChild(divString.firstChild);
+        divNote.removeChild(divNote.firstChild);
     }
-    divString.insertAdjacentHTML('afterbegin', `<h2 class="current-string text-center">${getStringAndNote()}</h2>`);
+    divString.insertAdjacentHTML('afterbegin', `<h3 class="current-string text-center">${getString()}</h3>`);
+    divNote.insertAdjacentHTML('afterbegin', `<h2 class="current-string text-center">${getNote()}</h2>`);
+
 };
 
 
 btnConfirmPractice.addEventListener('click', () => {
     // interval = prompt("Set interval in miliseconds");
     interval = parseInt(nrOfSeconds.value);
+    // TODO: if value empty break
     interval *= 1000;
     btnStartPractice.classList.add('d-none');
     btnStopPractice.classList.remove('d-none');
     cardString.classList.remove('d-none');
-    divString.insertAdjacentHTML('afterbegin', `<h2 class="current-string text-center">${getStringAndNote()}</h2>`);
+    divString.insertAdjacentHTML('afterbegin', `<h3 class="current-string text-center">${getString()}</h3>`);
+    divNote.insertAdjacentHTML('afterbegin', `<h2 class="current-string text-center">${getNote()}</h2>`);
     practiceInterval = setInterval(startPractice, interval);
 
     // let btnStopPractice = document.getElementById("stop-practice");
@@ -47,6 +54,7 @@ btnStopPractice.addEventListener('click', () => {
     cardString.classList.add('d-none');
     btnStartPractice.classList.remove('d-none');
     divString.removeChild(divString.firstChild);
+    divNote.removeChild(divNote.firstChild);
 })
 // divString.innerHTML = setInterval(() => {
 // }, 1000);    
@@ -56,14 +64,27 @@ btnStopPractice.addEventListener('click', () => {
 //     clearInterval(btnStartPractice);
 // })
 
-function getStringAndNote() {
+function getString() {
     let stringNumbers = '123456';
     let stringNumber = Math.floor(Math.random() * (stringNumbers.length)) + 1;
+    let stringMap = new Map([
+        [1, 'st'],
+        [2, 'nd'],
+        [3, 'rd'],
+        [4, 'th'],
+        [5, 'th'],
+        [6, 'th']
+    ]);
+    
+    //let stringNote = stringNotes[Math.floor(Math.random() * (stringNotes.length)) + 1];
+    return `${stringNumber}${stringMap.get(stringNumber)} string`
+};
+
+function getNote() {
     let stringNotesLength = stringNotes.length - 1;
     let index = Math.trunc(Math.random() * stringNotesLength);
     let stringNote = stringNotes[index];
-    //let stringNote = stringNotes[Math.floor(Math.random() * (stringNotes.length)) + 1];
-    return stringNumber + stringNote;
+    return stringNote
 };
 
 function highlightText(strings, ...values) {
